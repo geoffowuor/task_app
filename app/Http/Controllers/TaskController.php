30ed58bd168ed22a52ce294f74c6cd2f19518c7d
geoffowuor <geoffreygroovy@gmail.com>
2026-03-30 13:25:50 +0300

    first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+
+
+class TaskController extends Controller
+{
+    public function index(){
+        return Task::all();
+
+    }
+
+    public function store(Request $request){
+        $task = Task::create($request -> all());
+        return response() ->json($task, 201);
+    
+    }
+
+    public function show($id){
+        return  Task::findOrFail($id);
+
+    }
+
+    public function update( Request $request, $id){
+        $task = Task::findOrFail($id);
+        $task -> update($request -> all());
+        return $task;
+    }
+
+    public function destroy($id){
+        Task::destroy($id);
+        return response()->json(['message' => 'Task Deleted']);
+
+    }
+}
